4d1319fb5bdbad30cfa0159bcb7abe1102ff3eac
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2021-12-17 10:45:39 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,20 @@
+data = {
+    "nodes": [{"data": {"id": "1"}},
+        {"data": {"id": "2",}},
+        {"data": {"id": "3",}},
+        {"data": {"id": "4",}},
+        {"data": {"id": "5",}},
+        {"data": {"id": "6",}},
+        {"data": {"id": "7",}},
+        {"data": {"id": "8",}},
+        {"data": {"id": "9",}}],
+
+    "edges": [{"data": {"id": "a", "source": "1", "target": "2"}},
+        {"data": {"id": "b", "source": "2", "target": "4"}},
+        {"data": {"id": "c", "source": "4", "target": "5"}},
+        {"data": {"id": "d", "source": "4", "target": "6"}},
+        {"data": {"id": "e", "source": "2", "target": "7"}},
+        {"data": {"id": "f", "source": "1", "target": "3"}},
+        {"data": {"id": "g", "source": "3", "target": "8"}},
+        {"data": {"id": "h", "source": "8", "target": "9"}}]
+}
